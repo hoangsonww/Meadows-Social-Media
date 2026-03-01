eaa47db68a6681b13905eb3221b520534ea686c5
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:05 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'Meadows-Social-Media';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
